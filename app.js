e8f7507fb2920fd483bc45f2f04c9dfcdd0cbef6
Abduzafar1202 <aa908870138@gmail.com>
2024-03-22 15:40:50 +0500

    4-oy 3-dars

---
+++ app.js
@@ -0,0 +1,7 @@
+let elNav = document.querySelector("section.button");
+let elBtn = document.querySelector(".img2");
+elBtn.addEventListener("click" , () => {
+    
+    elNav.classList.toggle("d-none")
+    
+})
